a9de03aaa552d02319f310a66cbcbea3ceaeff54
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-10-29 14:02:09 +0100

    fix: array of media and links

--- src/exercise/inputs/add-exercise.dto.ts
+++ src/exercise/inputs/add-exercise.dto.ts
@@ -8,8 +8,8 @@ import {
   IsString,
 } from 'class-validator';
 import { MUSCLE_GROUP } from '../exercise.constants';
-import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class AddExerciseDto {
   @IsString()
@@ -19,15 +19,21 @@ export class AddExerciseDto {
   @IsEnum(MUSCLE_GROUP)
   muscleGroup: MUSCLE_GROUP;
 
-  @ApiProperty({ type: 'string', format: 'binary' })
-  @Allow()
-  media?: string[];
-
+  @ApiProperty({ isArray: true, type: String, format: 'binary' })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @ArrayNotEmpty()
-  @Type(() => Array)
+  @Transform(({ value }) => (Array.isArray(value) ? value : Array(value)))
+  @Allow()
+  media?: string[];
+
+  @ApiProperty({ isArray: true, type: String, required: false })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayNotEmpty()
+  @Transform(({ value }) => (Array.isArray(value) ? value : Array(value)))
   links?: string[];
 
   @IsString()
