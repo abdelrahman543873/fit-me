2669fe546e680824935b17846bae5d32b3d72f22
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-10-31 16:31:56 +0100

    fix: bearer

--- src/workout-plan/workout-exercise.controller.ts
+++ src/workout-plan/workout-exercise.controller.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { WorkoutExerciseService } from './workout-exercise.service';
 import { AddWorkoutExerciseDto } from './inputs/add-workout-exercise.dto';
 import { RequestInBodyInterceptor } from '../shared/interceptors/request-in-body.interceptor';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('workoutExercise')
 @Controller('workout-exercise')
 export class WorkoutExerciseController {
   constructor(private workoutExerciseService: WorkoutExerciseService) {}
