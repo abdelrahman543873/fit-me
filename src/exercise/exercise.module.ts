f96bf5e4c611c428ca23ec8cac6e3e9325f38999
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-11-02 16:14:34 +0100

    feat: added delete many prehook for exercise

--- src/exercise/exercise.module.ts
+++ src/exercise/exercise.module.ts
@@ -2,18 +2,37 @@ import { Module } from '@nestjs/common';
 import { ExerciseController } from './exercise.controller';
 import { ExerciseService } from './exercise.service';
 import { ExerciseRepository } from './exercise.repository';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Exercise, ExerciseSchema } from './exercise.schema';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { filename } from '../shared/utils/multer-file-name';
 import { ExistingExerciseValidator } from './validators/existing-exercise.validator';
 import { ExerciseOwnerValidator } from './validators/exercise-owner.validator';
+import {
+  WorkoutExercise,
+  WorkoutExerciseSchema,
+} from '../workout-exercise/workout-exercise.schema';
+import { Model } from 'mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Exercise.name, schema: ExerciseSchema },
+    MongooseModule.forFeatureAsync([
+      {
+        name: WorkoutExercise.name,
+        useFactory: () => WorkoutExerciseSchema,
+      },
+      {
+        name: Exercise.name,
+        useFactory: (workoutExercise: Model<WorkoutExercise>) => {
+          ExerciseSchema.pre('deleteOne', async function () {
+            const doc = await this.model.findOne(this.getFilter());
+            await workoutExercise.deleteMany({ exercise: doc._id });
+          });
+          return ExerciseSchema;
+        },
+        inject: [getModelToken(WorkoutExercise.name)],
+      },
     ]),
     MulterModule.register({
       preservePath: true,
--- test/exercise/delete-exercise.spec.ts
+++ test/exercise/delete-exercise.spec.ts
@@ -5,16 +5,27 @@ import { userFactory } from '../user/user.factory';
 import { exerciseFactory } from './exercise.factory';
 import { EXERCISE } from '../endpoints/exercise.endpoints';
 import { DeleteExerciseDto } from '../../src/exercise/inputs/delete-exercise.dto';
+import { workoutExerciseFactory } from '../workout-exercise/workout-exercise.factory';
+import { WorkoutExerciseRepo } from '../workout-exercise/workout-exercise.test-repo';
 
 describe('delete exercise suite case', () => {
   it('should delete exercise successfully', async () => {
     const trainer = await userFactory({ role: USER_ROLE.TRAINER });
     const exercise = await exerciseFactory({ trainer: trainer._id });
+    await workoutExerciseFactory({ exercise: exercise._id });
+    const workoutExerciseCount = await WorkoutExerciseRepo().count({
+      exercise: exercise._id,
+    });
+    expect(workoutExerciseCount).toBe(1);
     const res = await testRequest<DeleteExerciseDto>({
       method: HTTP_METHODS_ENUM.DELETE,
       url: `${EXERCISE}/${exercise._id.toString()}`,
       token: trainer.token,
     });
     expect(res.body.deletedCount).toBe(1);
+    const workoutExerciseCountAfterDelete = await WorkoutExerciseRepo().count({
+      exercise: exercise._id,
+    });
+    expect(workoutExerciseCountAfterDelete).toBe(0);
   });
 });
