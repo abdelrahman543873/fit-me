b58a046b6ea930b6d15ef388814af56488036fd1
Abdelrahman mysara <abdelrahmanmysara33@gmail.com>
2023-11-03 07:38:46 +0100

    Update program.controller.ts

--- src/program/program.controller.ts
+++ src/program/program.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Request } from '@nestjs/common';
 import { ProgramService } from './program.service';
 import { AddProgramDto } from './inputs/add-program.dto';
-import { RequestContext } from '../../dist/shared/interfaces/request-context.interface';
+import { RequestContext } from './../shared/interfaces/request-context.interface';
 
 @Controller('program')
 export class ProgramController {
