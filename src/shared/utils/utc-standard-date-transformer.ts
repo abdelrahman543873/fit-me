f32d128b57c7c877057ab84f66a2c4b8a5e9fbd3
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2023-12-14 00:44:07 +0100

    hotfix: date transoformer

--- src/shared/utils/utc-standard-date-transformer.ts
+++ src/shared/utils/utc-standard-date-transformer.ts
@@ -2,7 +2,7 @@ export const utcStandardDateTransformer = (input) => {
   const cleanedInput = input.value.replace(/["]/g, '');
   if (new Date(cleanedInput)) {
     const date = new Date(cleanedInput);
-    date.setHours(24, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
     return date;
   }
   return input.value;
